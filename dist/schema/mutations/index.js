9fe4c42e241eaf38d84251b174827c04e2626d09
Darshan Bajeja <codingdsb@gmail.com>
2021-11-13 15:57:12 +0530

    remove dist from working tree

--- dist/schema/mutations/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.REGISTER = void 0;
-var User_1 = require("./User");
-Object.defineProperty(exports, "REGISTER", { enumerable: true, get: function () { return User_1.REGISTER; } });
-//# sourceMappingURL=index.js.map
--- dist/schema/queries/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.LOGIN = void 0;
-var User_1 = require("./User");
-Object.defineProperty(exports, "LOGIN", { enumerable: true, get: function () { return User_1.LOGIN; } });
-//# sourceMappingURL=index.js.map
