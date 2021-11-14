1d063c9dfaf2ee46383934b073e1e9ef1f16a199
Darshan Bajeja <codingdsb@gmail.com>
2021-11-14 17:09:10 +0530

    rejectUnauthorized false for db connection in production

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,9 @@ const main = async () => {
     entities: [User, Transaction],
     logging: !__prod__,
     synchronize: true,
-    ssl: __prod__,
+    ssl: {
+      rejectUnauthorized: __prod__,
+    },
   });
 
   const app = express();
